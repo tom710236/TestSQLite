aefde18cb2fc0fbe48fac1242415b1654dc9cb37
TOM <tom710236@gmail.com>
2017-03-07 17:44:02 +0800

    SQL-查詢

--- app/src/main/java/com/example/tom/testsqlite/MainActivity.java
+++ app/src/main/java/com/example/tom/testsqlite/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.tom.testsqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         //開啟或建立資料庫 (參數1 資料庫名稱 參數2 內建參數(此資料庫要給那些人使用) 參數3 ErrorHandler)
         db=openOrCreateDatabase(db_name, Context.MODE_PRIVATE, null);
        /*
-        *建立資料表
+        *建立資料表內容
         *用SQL語法"CREATE TABLE IF NOT EXISTS "
         *不存在時才建立
         * */
@@ -37,30 +39,54 @@ public class MainActivity extends AppCompatActivity {
                 + "phone" + " INTEGER PRIMARY KEY,"
                 + "email" + " TEXT"
                 + ")";
-        //呼叫execSQL 執行 createTable
+        //呼叫execSQL 執行 createTable 建立資料表
         db.execSQL(createTable);
-        //呼叫自訂的addData方法 在資料表新增兩筆紀錄
-        addData("tom","0220019891","tom@yahoo.com.tw");
-        addData("Mary","022934198","Mary@yahoo.com.tw");
-        //顯示資料庫一些基本資料
-        TextView txv=(TextView)findViewById(R.id.textView);
-        txv.setText("資料庫路徑: "+db.getPath()+"\n"+
-                    "資料庫分頁大小: "+db.getPageSize()+"Bytes\n"+
-                    "資料量上限: "+db.getMaximumSize()+"Bytes\n");
+        //呼叫rawQuery()執行SELECT查詢敘述 並將結果設定給Cursor物件c
+        Cursor c =db.rawQuery("SELECT * FROM " +tb_name,null);
+        /*
+        *檢查查詢結果資料筆數是否為0 若是為0(沒有資料)就執行addData新增資料
+        *新增後再用db.rawQuery重新執行查詢
+        **/
+        if (c.getCount()==0){
+            addData("tom","0220019891","tom@yahoo.com.tw");
+            addData("Mary","022934198","Mary@yahoo.com.tw");
+            c =db.rawQuery("SELECT * FROM " +tb_name,null);
+        }
+        /*
+        * 再有資料的情況下
+        * 用moveFirst逐步檢查
+        * 並顯示在TextView
+        * */
+
+        if(c.moveToFirst()){
+            String str = "總共有 "+c.getCount()+"筆資料\n";
+            str+="-----\n";
+            Log.e("str",str);
+            Log.e("c.getCount", String.valueOf(c.getCount()));
+
+            do{
+                        str+="name:"+c.getString(0)+"\n";
+                        str+="phone:"+c.getString(1)+"\n";
+                        str+="email"+c.getString(2)+"\n";
+                        Log.e("str2",str);
+
+                    str+="-----\n";
+
+            //有下一筆就繼續查詢(moveToNext)
+            }while (c.moveToNext());
+
+            TextView textView = (TextView)findViewById(R.id.textView);
+            textView.setText(str);
+        }
         db.close();
+
     }
-    /*
-     *自訂的方法
-     *把三個參數字串加到資料表中成為一筆紀錄
-     *建立含3個項目的ContentValues
-     *並用put()方法將其字串加到物件中
-     *
-     * */
     private void addData(String name,String phone,String email){
         ContentValues cv = new ContentValues(3);
-        cv.put("naem",name);
+        cv.put("name",name);
         cv.put("phone",phone);
-        cv.put("emial",email);
+        cv.put("email",email);
+        Log.e("CV", String.valueOf(cv));
         //將資料加到資料表
         db.insert(tb_name,null,cv);
     }
